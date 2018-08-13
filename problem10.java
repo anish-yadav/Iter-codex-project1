e4400e2debe4acd40167411855aceb7657795e87
Anish Kumar Yadav <40102074+anish-yadav@users.noreply.github.com>
2018-08-13 23:56:39 +0530

    improved the code to load faster

--- problem10.java
+++ problem10.java
@@ -1,25 +1,29 @@
 public class problem10
 {
+   static boolean primechecker(long n)
+    {
+        boolean status = true;
+        for (int i = 3; i<=Math.sqrt(n);i+=2)
+        {
+            if (n%i==0)
+            {
+                status=false;
+                break;
+            }
+        }
+        return status;
+    }
     public static void main(String args[])
     {
-        int i =0;
-      int num =0,sum=0;
-      int n = 20000000;
-      for (i = 1; i <= n; i++)  	   
-      { 		 		  
-         int counter=0; 		  
-         for(num =i; num>=1; num--)
-         {
-	    if(i%num==0)
-	    {
-		counter = counter + 1;
-	    }
-	 }
-	 if (counter ==2)
-	 {
-	    sum = sum +i;
-	 }	
-      }	
-      System.out.println(sum);
+        
+        long sum = 0;
+        for(long i = 3 ;i<=2000000;i+=2) // no even number can be prime except 2
+        {
+            if(primechecker(i))
+            {
+                sum = sum +i;
+            }
+        }
+        System.out.println(sum+2); //because 2 is not included
    }
 }
