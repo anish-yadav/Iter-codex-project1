c345f82ef768ec4f7710b1eedc585288a018242b
Anish Kumar Yadav <40102074+anish-yadav@users.noreply.github.com>
2018-08-13 17:21:58 +0530

    Update problem2.java

--- problem2.java
+++ problem2.java
@@ -1,23 +1,20 @@
-import java.io.*;
 public class problem2
 {
-    public static void main(String args[]) throws IOException
+    public static void main(String args[]) 
     {
         long num1=0,num2=1,temp,sum=0;
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the number till which you want to get the sum : ");
-        long num = Long.parseLong(buff.readLine());
+        long num = 4000000;
         while(num2<num)
         {
           temp=num2;
           num2=num1+num2;
           num1=temp;
-          //System.out.print(num2+" , ");
+         
           if(num2%2==0)
           {
               sum=sum+num2;
             }
         }
-        System.out.print("The sum of even numbers of the fibonacci series is : "+sum);
+        System.out.print(sum);
     }
 }
