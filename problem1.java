9e12e05720f9275e3de4842ef36c03873f61437e
Anish Kumar Yadav <40102074+anish-yadav@users.noreply.github.com>
2018-08-13 17:19:02 +0530

    simplified and corrected it

--- problem1.java
+++ problem1.java
@@ -1,28 +1,15 @@
-import java.io.*;
+
 public class problem1
 {
-    int sum=0;
-    int multiple(int n , int check)
-    {
-        for(int i = check; i<n; i++)
-        {
-            if(i%check==0)
-            {
-            sum=sum+i; 
-           // System.out.print(i+" , ");
-           //k System.out.println(sum+" , ");
-            }
-        }
-       return sum;
-    }
-   public static void main(String args[]) throws IOException
+   public static void main(String args[]) 
    {
-       BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-       System.out.print("Enter the number below which you would like to find the sum of multiples of 3 & 5 :");
-       int num=Integer.parseInt(buff.readLine());
-       problem1 obj = new problem1();
-       int sum3 = obj.multiple(num,3);
-       int sum5 = obj.multiple(num,5);
-       System.out.println("The sum of multiple of 3 and 5 below "+num+" is "+sum5);
+        int num = 1000;
+       int sum =0;
+       for(int i = 0;i<num;i++)
+       {
+           if((i%5==0)||(i%3==0))
+           sum+=i;
+        }
+       System.out.println(sum);
     }
 }
